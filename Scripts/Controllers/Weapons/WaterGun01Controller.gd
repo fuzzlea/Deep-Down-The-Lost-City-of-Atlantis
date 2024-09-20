4c1580503fc467ebd936fd194103ae22d5908d7f
aj <fuzzleaofficial@gmail.com>
2024-09-20 09:00:58 -0400

    03

--- Scripts/Controllers/Weapons/WaterGun01Controller.gd
+++ Scripts/Controllers/Weapons/WaterGun01Controller.gd
@@ -1,1 +1,24 @@
 extends Sprite2D
+
+# Export #
+
+@export var Type : String = "Gun"
+
+# Vars #
+
+# Func #
+
+func setRotation():
+	var mouseVector : Vector2 = get_local_mouse_position()
+	
+	if mouseVector.x < 0 :
+		# facing left
+		flip_h = true
+	else:
+		#facing right
+		flip_h = false
+
+# Connectors #
+
+func _process(_delta):
+	setRotation()
